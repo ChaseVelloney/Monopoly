3470992e5c891dadf046e2b36412973a6e351044
ChaseVelloney <crv37@calvin.edu>
2024-11-20 17:02:46 -0500

    HW03 #3

--- monopolyService.js
+++ monopolyService.js
@@ -1,34 +1,65 @@
 /* eslint-disable no-template-curly-in-string */
 /* eslint-disable no-console */
 /* eslint-disable no-use-before-define */
+/**
+ * This module implements a REST-inspired webservice for the Monopoly DB.
+ * The database is hosted on ElephantSQL.
+ *
+ * Currently, the service supports the player table only.
+ *
+ * To guard against SQL injection attacks, this code uses pg-promise's built-in
+ * variable escaping. This prevents a client from issuing this URL:
+ *     https://cs262-webservice.azurewebsites.net//players/1%3BDELETE%20FROM%20PlayerGame%3BDELETE%20FROM%20Player
+ * which would delete records in the PlayerGame and then the Player tables.
+ * In particular, we don't use JS template strings because it doesn't filter
+ * client-supplied values properly.
+ * TODO: Consider using Prepared Statements.
+ *      https://vitaly-t.github.io/pg-promise/PreparedStatement.html
+ *
+ * This service assumes that the database connection strings and the server mode are
+ * set in environment variables. See the DB_* variables used by pg-promise. And
+ * setting NODE_ENV to production will cause ExpressJS to serve up uninformative
+ * server error responses for all errors.
+ *
+ * @author: kvlinden
+ * @date: Summer, 2020
+ */
+
+// Set up the database connection.
 
 const pgp = require('pg-promise')();
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const router = express.Router();
-router.use(express.json());
 
 const db = pgp({
   host: process.env.DB_SERVER,
-  port: process.env.DB_PORT,
+  port: 5432, // Port set to 5432
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: {rejectUnauthorized: false},
+  ssl: { rejectUnauthorized: false },
 });
 
-// Define routes
-router.get('/', readHelloMessage);
-router.get('/players', readPlayers);
-router.get('/players/:id', readPlayer);
-router.get('/playerScores', readPlayerScores); // New endpoint
-router.put('/players/:id', updatePlayer);
-router.post('/players', createPlayer);
-router.delete('/players/:id', deletePlayer);
+// Configure the server and its routes.
 
-app.use(router);
+const express = require('express');
+const cors = require('cors'); // Add CORS package
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json());
+
+// Define routes
+app.get('/', readHelloMessage);
+app.get('/players', readPlayers);
+app.get('/players/:id', readPlayer);
+app.get('/playerScores', readPlayerScores); // New endpoint for join query
+app.put('/players/:id', updatePlayer);
+app.post('/players', createPlayer);
+app.delete('/players/:id', deletePlayer);
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Implement the CRUD operations.
 
 function returnDataOr404(res, data) {
   if (data == null) {
@@ -77,7 +108,7 @@ function readPlayerScores(req, res, next) {
 }
 
 function updatePlayer(req, res, next) {
-  db.oneOrNone('UPDATE Player SET email=${body.email}, name=${body.name} WHERE id=${params.id} RETURNING id', req)
+  db.oneOrNone('UPDATE Player SET email=${body.email}, name=${body.name}, cash=${body.cash}, position=${body.position} WHERE id=${params.id} RETURNING id', req)
     .then((data) => {
       returnDataOr404(res, data);
     })
@@ -87,7 +118,7 @@ function updatePlayer(req, res, next) {
 }
 
 function createPlayer(req, res, next) {
-  db.one('INSERT INTO Player(email, name) VALUES (${email}, ${name}) RETURNING id', req.body)
+  db.one('INSERT INTO Player(email, name, cash, position) VALUES (${email}, ${name}, ${cash}, ${position}) RETURNING id', req.body)
     .then((data) => {
       res.send(data);
     })
