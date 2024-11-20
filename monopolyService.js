fe2ed704620ca334fb98367f7f6f6dd90103911c
ChaseVelloney <crv37@calvin.edu>
2024-11-20 17:07:23 -0500

    HW03 #4

--- monopolyService.js
+++ monopolyService.js
@@ -1,47 +1,16 @@
-/* eslint-disable no-template-curly-in-string */
-/* eslint-disable no-console */
-/* eslint-disable no-use-before-define */
-/**
- * This module implements a REST-inspired webservice for the Monopoly DB.
- * The database is hosted on ElephantSQL.
- *
- * Currently, the service supports the player table only.
- *
- * To guard against SQL injection attacks, this code uses pg-promise's built-in
- * variable escaping. This prevents a client from issuing this URL:
- *     https://cs262-webservice.azurewebsites.net//players/1%3BDELETE%20FROM%20PlayerGame%3BDELETE%20FROM%20Player
- * which would delete records in the PlayerGame and then the Player tables.
- * In particular, we don't use JS template strings because it doesn't filter
- * client-supplied values properly.
- * TODO: Consider using Prepared Statements.
- *      https://vitaly-t.github.io/pg-promise/PreparedStatement.html
- *
- * This service assumes that the database connection strings and the server mode are
- * set in environment variables. See the DB_* variables used by pg-promise. And
- * setting NODE_ENV to production will cause ExpressJS to serve up uninformative
- * server error responses for all errors.
- *
- * @author: kvlinden
- * @date: Summer, 2020
- */
-
-// Set up the database connection.
-
 const pgp = require('pg-promise')();
+const express = require('express');
+const cors = require('cors');
 
 const db = pgp({
   host: process.env.DB_SERVER,
-  port: 5432, // Port set to 5432
+  port: 5432, // Port for PostgreSQL
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   ssl: { rejectUnauthorized: false },
 });
 
-// Configure the server and its routes.
-
-const express = require('express');
-const cors = require('cors'); // Add CORS package
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -49,90 +18,78 @@ app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 // Define routes
-app.get('/', readHelloMessage);
-app.get('/players', readPlayers);
-app.get('/players/:id', readPlayer);
-app.get('/playerScores', readPlayerScores); // New endpoint for join query
-app.put('/players/:id', updatePlayer);
-app.post('/players', createPlayer);
-app.delete('/players/:id', deletePlayer);
+app.get('/', (req, res) => {
+  res.send('Hello, CS 262 Monopoly service!');
+});
+
+app.get('/players', async (req, res, next) => {
+  try {
+    const data = await db.many('SELECT id, emailaddress AS email, name, cash, position FROM Player');
+    res.send(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/players/:id', async (req, res, next) => {
+  try {
+    const data = await db.oneOrNone('SELECT * FROM Player WHERE id=${id}', req.params);
+    if (data) {
+      res.send(data);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/playerScores', async (req, res, next) => {
+  try {
+    const data = await db.many(`
+      SELECT Player.name, Player.emailAddress, PlayerGame.score
+      FROM Player
+      JOIN PlayerGame ON Player.ID = PlayerGame.playerID
+    `);
+    res.send(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/players/:id', async (req, res, next) => {
+  try {
+    const data = await db.oneOrNone('UPDATE Player SET email=${body.email}, name=${body.name}, cash=${body.cash}, position=${body.position} WHERE id=${params.id} RETURNING id', req);
+    if (data) {
+      res.send(data);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/players', async (req, res, next) => {
+  try {
+    const data = await db.one('INSERT INTO Player(email, name, cash, position) VALUES (${email}, ${name}, ${cash}, ${position}) RETURNING id', req.body);
+    res.send(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.delete('/players/:id', async (req, res, next) => {
+  try {
+    const data = await db.oneOrNone('DELETE FROM Player WHERE id=${id} RETURNING id', req.params);
+    if (data) {
+      res.send(data);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// Implement the CRUD operations.
-
-function returnDataOr404(res, data) {
-  if (data == null) {
-    res.sendStatus(404);
-  } else {
-    res.send(data);
-  }
-}
-
-function readHelloMessage(req, res) {
-  res.send('Hello, CS 262 Monopoly service!');
-}
-
-function readPlayers(req, res, next) {
-  db.many('SELECT id, emailaddress AS email, name, cash, position FROM Player')
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
-
-function readPlayer(req, res, next) {
-  db.oneOrNone('SELECT * FROM Player WHERE id=${id}', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
-
-function readPlayerScores(req, res, next) {
-  db.many(`
-    SELECT Player.name, Player.emailAddress, PlayerGame.score
-    FROM Player
-    JOIN PlayerGame ON Player.ID = PlayerGame.playerID
-  `)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
-
-function updatePlayer(req, res, next) {
-  db.oneOrNone('UPDATE Player SET email=${body.email}, name=${body.name}, cash=${body.cash}, position=${body.position} WHERE id=${params.id} RETURNING id', req)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
-
-function createPlayer(req, res, next) {
-  db.one('INSERT INTO Player(email, name, cash, position) VALUES (${email}, ${name}, ${cash}, ${position}) RETURNING id', req.body)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
-
-function deletePlayer(req, res, next) {
-  db.oneOrNone('DELETE FROM Player WHERE id=${id} RETURNING id', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
